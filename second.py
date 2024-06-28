e86ddb5e914b5441794a7d4cd8b3d9bc5c8ae88a
Balaji251226 <kgbalaji45@gmail.com>
2024-06-29 00:07:33 +0530

    updated

--- second.py
+++ second.py
@@ -1,7 +1,7 @@
 num = int(input("Enter Num :"))
 if (num % 2 == 0):  #2 vala divide pana vara reminder zero va iruntha even illana odd
-    print("Odd")  
+    print("Even")  
 else:
-    print("Even")
+    print("Odd")
 
 print("Even" if(num % 2 == 0) else "Odd")
